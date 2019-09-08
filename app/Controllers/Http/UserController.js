36bdf4fe03561ba21d2ff6941643d84c9488fb77
febridev <febriyantabidin@gmail.com>
2019-09-08 13:42:58 +0700

    filter login

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,26 @@
 'use strict'
+const { validate } = use('Validator')
 
 class UserController {
-    async login ({request, auth}){
+    async login ({request, auth, session, response }){
+        const rules = {
+            password: 'required'
+        }
+        const validation = await validate(request.all(),rules)
+        if (validation.fails()) {
+            session 
+                .withErrors(validation.messages())
+                .flashAll()
+            return response.redirect('back')
+        }
         try {
             const{email, password} = request.all()
             await auth.attempt(email,password)
-        
-            return 'Bershasil login'
+            return 'Bershasil login'  
         } catch (error) {
-            console.log(error)    
+            console.log(error)
+            session.flash({notification: 'Error Login #1, Check Email or Password Please '})
+            return response.redirect('back')
         }
     }
 }
